e382f77969ebf81922b0c77271cc133c0616ac99
Chris Simpson <chrisrsimpson@gmail.com>
2018-04-07 01:19:01 +1000

    Auto commit

--- AttendanceReport.ps1
+++ AttendanceReport.ps1
@@ -13,13 +13,21 @@ $BlackList = cat $blacklistfile
 $global:raidJoinCSV = import-csv ($DBSub + "join.csv") | ?{$_.name -eq "$name" -and $blacklist -notcontains $_.date } # -and (get-date $_.date).DayOfWeek } | sort-object -Property date
 $global:raidLeaveCSV = import-csv ($DBSub + "leave.csv") | ?{$_.name -eq "$name" -and $blacklist -notcontains $_.date} | sort-object -Property date
 
+
+[datetime]$dateformatting = $entry.date.Replace('.', '/')
+
 [datetime]$firstRaid = $raidJoinCSV | select-object -First 1 | select -ExpandProperty date
 [datetime]$latestRaid = $raidJoinCSV | select-object -last 1 | select -ExpandProperty date
 
+
+
+
+
+
+$expectedRaids = [math]::Round($weeksbetween * $raidsPerWeek)
 $totalRaids = ($raidJoinCSV | Select-Object -Property date -Unique).Count
 $weeksbetween = ((New-TimeSpan -Start $firstRaid -End $latestRaid | select -ExpandProperty Days) / 7)
 
-$expectedRaids = [math]::Round($weeksbetween * $raidsPerWeek)
 
 
 
