5272ebd3fa05d81445f75c8f16ea7dea7993053e
Chris Simpson <chrisrsimpson@gmail.com>
2018-04-08 04:00:05 +1000

    Auto commit

--- raid.ps1
+++ raid.ps1
@@ -1,76 +1,68 @@
-#Looking for -Raid flag
 
-if ($raid) {
-    raidfunction -Raidentry $raid
+$ConfigFile = "D:\dropbox\guildlua\config_Lua.xml"
+[xml]$Config = Get-Content $ConfigFile
+$freshrun = "yes"
+
+#Pre-Requisite checks for existing data. Creates directories if they do not exist
+
+$DBSub = $Config.settings.baseconfig.workingdir + '\' + $Config.settings.baseconfig.databasefolder + '\'
+$RPSub = $Config.settings.baseconfig.workingdir + '\' + $Config.settings.reporting.reportfolder + '\'
+<#Logic for raid function: 
+
+
+Get joins, sort by date, add to array
+Get leaves, same
+Get loot, same
+#>
+
+function Create-RaidLists { 
+    $script:joinlist = New-Object -TypeName System.Collections.ArrayList
+    $script:leavelist = New-Object -TypeName System.Collections.ArrayList
+    $script:lootlist = New-Object -TypeName System.Collections.ArrayList
+    $script:namelist = New-Object -TypeName System.Collections.ArrayList
+
 }
-function checkLastLoot($lootName) {
-    return $lootarray | ? {($_.name -eq $lootname) -and ($_.priority -ge $config.settings.reporting.qualityfilter)} | sort-object date | Select-Object -Last $quantity
-}
-   
-function RaidFunction($Raidentry) {
+$joins = (import-csv ($dbsub + "join.csv")) | Sort-Object -Property date
+$leaves = import-csv ($dbsub + "leave.csv")
+$loots = import-csv ($dbsub + "loot.csv")
 
-   
-    #Begin processing of ALL raid reports
-    if ($Raidentry -eq '*') {
-        $joinImport = (import-csv ($dbsub + 'join.csv')) | Sort-Object -Property date -Descending
-        $leaveImport = (import-csv ($dbsub + 'leave.csv')) | Sort-Object -Property date -Descending
-        $lootImport = (import-csv ($dbsub + 'loot.csv')) | Sort-Object -Property date -Descending
-        "uyo"
-        #3 Arrays for holding stuff
-        $joinArray = New-Object System.Collections.ArrayList($null)
-        $leaveArray = New-Object System.Collections.ArrayList($null)
-        $lootArray = New-Object System.Collections.ArrayList($null)
+Create-RaidLists
 
-        #Now we have everything sorted by date, lets pump everything into arraylists and initiate the swap when the date changes
-        foreach ($join in $joinimport) { 
-            if (($join.datestamp -ne $datestamp) -and ($datestamp)) {
-                $raidreportfilename = $raidreportfolder + 'RaidReport_' + $datestamp + '.csv'
-                $joinArray | export-csv $raidreportfilename -NoTypeInformation
-                $joinArray = New-Object System.Collections.ArrayList($null)
-            }
-            [void]$joinarray.Add($join)
-            $datestamp = $join.datestamp 
-        }
+foreach ($join in $joins) {
+    #$join
+#finding the block where the entry where the date changes to start processing each file
+    if (($join.date -ne $lastdate) -and ($lastdate -ne $null)) {
+        [void]$leavelist.Add(($leaves | ?{$_.date -eq $lastdate}))
+        [void]$lootlist.Add(($loot | ?{$_.date -eq $lastdate}))
+
+        #Now we have all the date related data stored, Lets generate each raid report based on who attended.
+        $leavelist | %{ [void]$namelist.add($_.name) }
+        $joinlist | %{ [void]$namelist.add($_.name) }
+        $lootlist | %{ [void]$namelist.add($_.name) }
+
+        $namelist = $namelist | select-object -unique
+        
     }
-    #Begin processing of individual raid reports
-    else {
-        $raidreportfilename = $raidreportfolder + 'RaidReport_' + $raidentry + '.csv'
-        $raidstore = New-Object System.Collections.ArrayList($null)
-        $joinImport = import-csv ($dbsub + 'join.csv') | ? {$_.date -like $raidentry}
-        $leaveImport = import-csv ($dbsub + 'leave.csv') | ? {$_.date -like $raidentry}
-        $lootImport = import-csv ($dbsub + 'loot.csv') | ? {$_.date -like $raidentry}
-        $namearray = New-Object System.Collections.ArrayList($null)
-        $namearray = $namearray | select -Unique
-        try {
-            $joinimport | % { [void]$namearray.Add($_.name) }
-            $leaveimport| % { [void]$namearray.Add($_.name) }
-            $lootimport| % { [void]$namearray.Add($_.name) }
-        } 
-        catch { }
-        foreach ($name in $namearray) { 
-            $lootlist = $null
-            $joinimport | ? {$_.name -eq "$name"} | sort-object -Property join | select-Object join -first 1
-            $leavetime = $leaveimport | ? {$_.name -eq "$name"} | sort-object -property leave | select-Object leave -last 1
-            $lootCollection = $lootimport | ? {$_.name -eq "$name"} | ? {$_.priority -ge $Config.settings.reporting.qualityfilter}
-            foreach ($loot in $lootcollection) {
-                if ($lootlist -eq $null) { $lootlist = $loot.item } else { $lootlist = $lootlist + ";" + $loot.item }
-                if ($URLlist -eq $null) { $URLlist = $loot.URL } else { $URLlist = $URLlist + ";" + $loot.URL }
-            }
-            if (!$leavetime.leave) { $perPersonLeave = "NO DATA" } else { $perPersonLeave = $leavetime.leave }
-            if (!$jointime.join) { $perPersonjoin = "NO DATA" }  Else { $perPersonJoin = $jointime.join }
-            $obj = [pscustomobject][ordered]@{
-                Name  = $name
-                Join  = $perPersonJoin
-                Leave = $perPersonLeave
-                Loot  = $lootlist
-                URL   = $URLList
-            }
-            $lootlist = $null
-            $urllist = $null
-            [void]$raidstore.Add($obj)
-
-        }
-        if (!$raidstore) { Throw "Error: No data found for that raid, Correct Syntax: YYYY-MM-DD" } 
-        else { $raidstore | export-csv $raidreportfilename }
-    }
+    else { [void]$joinlist.add($join) ; $lastdate = $join.date}
 }
+$leavelist
+
+
+<#
+if ($_.date -ne $lastdate) { 
+leavearray.add($Leaves | ?{$_.date -eq $lastdate})
+lootarray.add($loot  | ?{$_.date -eq $lastdate})
+$namearray = new arraylist
+foreach ($Name in $arrays 1 2 3) { $namearray.ADd($name) }
+}
+select unique
+csv name = lastdate
+
+foreach ($name in $namearray) {
+
+}
+
+
+$lastdate = $_.date
+}
+#>
