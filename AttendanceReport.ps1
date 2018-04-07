54b91113b10fef6ec7fb3f46f276c6ebcc47d793
Chris Simpson <chrisrsimpson@gmail.com>
2018-04-07 12:18:15 +1000

    Auto commit

--- AttendanceReport.ps1
+++ AttendanceReport.ps1
@@ -1,27 +1,21 @@
-$ConfigFile = "D:\dropbox\guildlua\config_Lua.xml"
-[xml]$Config = Get-Content $ConfigFile
-#attendance report
-$name = "Sodanickel"
-[int]$raidsPerWeek = 2
-$DBSub = $Config.settings.baseconfig.workingdir + '\' + $Config.settings.baseconfig.databasefolder + '\'
-$raiddays = $Config.settings.reporting.Raiddays.Split(",") 
 $blacklistfile = $DBsub + 'blacklist.txt'
 if (!(test-path $blacklistfile)) { New-Item $blacklistfile -ItemType file }
 $BlackList = cat $blacklistfile
 
 
-$global:raidJoinCSV = import-csv ($DBSub + "join.csv") | ?{$_.name -eq "$name" -and $blacklist -notcontains $_.date } | sort-object -Property date
-$global:raidLeaveCSV = import-csv ($DBSub + "leave.csv") | ?{$_.name -eq "$name" -and $blacklist -notcontains $_.date} | sort-object -Property date
+$raidJoinCSV = import-csv ($DBSub + "join.csv") | ?{$_.name -eq "$name" -and $blacklist -notcontains $_.date } | sort-object -Property date
+$raidLeaveCSV = import-csv ($DBSub + "leave.csv") | ?{$_.name -eq "$name" -and $blacklist -notcontains $_.date} | sort-object -Property date
 
-$raidjoin = foreach ($entry in $raidjoincsv) {
+$global:raidjoin = foreach ($entry in $raidjoincsv) {
     #$entry.date
     [datetime]$dateformatting = $entry.date.Replace('.', '/')
+    $dateformatting.dayofweek
        if (($raiddays -contains $dateformatting.dayofweek) -and ($Config.settings.reporting.raidtimeonly -eq "true")) {
     $entry
     }
     if ($Config.settings.reporting.raidtimeonly -ne "true") { $entry }
 }
-
+$raidjoin | export-csv test.csv
 [datetime]$firstRaid = $raidJoin | select-object -First 1 | select -ExpandProperty date
 [datetime]$latestRaid = $raidJoin | select-object -last 1 | select -ExpandProperty date
 
@@ -30,7 +24,6 @@ $weeksbetween = ((New-TimeSpan -Start $firstRaid -End $latestRaid | select -Expa
 $expectedRaids = [math]::Round($weeksbetween * $raidsPerWeek - $blacklist.count)
 $totalRaids = ($raidJoin | Select-Object -Property date -Unique).Count
 
-sss
 
 
 
